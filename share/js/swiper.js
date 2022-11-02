c69f28873970278d0d2c2f8520805ac4a8f2ea93
UsAll-100designer <100designer@kakao.com>
2022-11-02 15:44:16 +0900

    Update

---
+++ share/js/swiper.js
@@ -0,0 +1,14 @@
+// var swiper = new Swiper('.mySwiper', {
+//   slidesPerView: 4,
+//   centeredSlides: true,
+//   spaceBetween: 30,
+//
+// });
+
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 3,
+  spaceBetween: 15,
+  slidesPerGroup: 3,
+  loopFillGroupWithBlank: true,
+  grabCursor: true,
+});
