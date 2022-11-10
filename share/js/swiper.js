125985e4a9939c2b7e4dfa37035c5d3021e51021
UsAll-100designer <100designer@kakao.com>
2022-11-10 11:20:38 +0900

    Update

--- share/js/swiper.js
+++ share/js/swiper.js
@@ -5,10 +5,41 @@
 //
 // });
 
+// 프로그래스 바
+// var swiper = new Swiper('.mySwiper', {
+//   slidesPerView: 3,
+//   spaceBetween: 0,
+//   slidesPerGroup: 1,
+//   loopFillGroupWithBlank: true,
+//   grabCursor: true,
+//   pagination: {
+//     el: '.swiper-pagination',
+//     type: 'progressbar',
+//   },
+// });
+
+//다이나믹 페이지네이션
+// var swiper = new Swiper('.mySwiper', {
+//   slidesPerView: 3,
+//   spaceBetween: 0,
+//   slidesPerGroup: 1,
+//   loopFillGroupWithBlank: true,
+//   grabCursor: true,
+//   pagination: {
+//     el: '.swiper-pagination',
+//     dynamicBullets: true,
+//   },
+// });
+
+//네비게이션
 var swiper = new Swiper('.mySwiper', {
   slidesPerView: 3,
   spaceBetween: 0,
   slidesPerGroup: 1,
   loopFillGroupWithBlank: true,
   grabCursor: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
