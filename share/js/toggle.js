d796ed7ce6e756d800a72c77d20f63b461a17947
UsAll-100designer <100designer@kakao.com>
2022-12-19 15:12:17 +0900

    Update

---
+++ share/js/toggle.js
@@ -0,0 +1,9 @@
+// jQuery
+$('.toggle-up').click(function (e) {
+  $('.toggle-up').toggleClass('active');
+  $('.function-top').toggleClass('up');
+});
+$('.toggle-down').click(function (e) {
+  $('.toggle-down').toggleClass('active');
+  $('.function-down').toggleClass('down');
+});
